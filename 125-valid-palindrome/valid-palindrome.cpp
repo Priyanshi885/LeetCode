fd81c67bb313c301d595dae822b90aed84e1f088
Priyanshi Saxena <saxenapriyanshi94@gmail.com>
2026-07-07 13:54:35 +0530

    Time: 1 ms (44.16%) | Memory: 10 MB (55.28%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -4,11 +4,11 @@ public:
         int left=0;
         int right = s.size()-1;
 
-        while(left<right){
-            while(left<right && !isalnum(s[left])){
+        while(left<right){ //keep checking the string until all pairs are compared
+            while(left<right && !isalnum(s[left])){ //keep moving left until you found a valid letter or digit
                 left++;
             }
-            while(left<right && !isalnum(s[right])){
+            while(left<right && !isalnum(s[right])){ // keep moving right until you found a valid letter or digit
                 right--;
             }
             
