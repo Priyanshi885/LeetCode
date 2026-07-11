6fe4885353690c058a41f9424e06d8be3f623b8e
Priyanshi Saxena <saxenapriyanshi94@gmail.com>
2026-07-11 22:39:49 +0530

    Time: 103 ms (55.33%) | Memory: 95.1 MB (98.80%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
+        
         unordered_map<int,int>mp;
         long long sum=0;
         long long ans=0;
+        int n=nums.size();
 
         //first window
         for(int i=0; i<k; i++){
@@ -11,12 +13,12 @@ public:
             mp[nums[i]]++;
         }
 
-        if(mp.size()==k)
-        ans = sum;
+        if(mp.size()==k){
+            ans=sum;
+        }
 
         //sliding window
-        for(int i=k; i<nums.size(); i++){
-            
+        for(int i=k; i<n; i++){
             //add new element
             sum+=nums[i];
             mp[nums[i]]++;
@@ -30,7 +32,7 @@ public:
 
             //check if all elements are distinct
             if(mp.size()==k)
-            ans=max(ans,sum);
+            ans= max(ans,sum);
         }
         return ans;
     }
