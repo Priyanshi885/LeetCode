85090ffcbfd3646380692d6605bf39d3003d7ecc
Priyanshi Saxena <saxenapriyanshi94@gmail.com>
2026-08-13 10:15:58 +0530

    Time: 79 ms (37.61%) | Memory: 131.2 MB (76.45%) - LeetSync

---
+++ 502-ipo/ipo.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        vector<pair<int,int>>projects;
+        for(int i=0; i<profits.size(); i++){
+            projects.push_back({capital[i],profits[i]});
+        }
+        sort(projects.begin(),projects.end());
+        priority_queue<int>pq;
+        int  i=0;
+        for(int project=0; project<k; project++){
+            while(i<projects.size() && projects[i].first<=w){
+                pq.push(projects[i].second);
+                i++;
+            }
+            if(pq.empty()){
+                break;
+            }
+            w+=pq.top();
+            pq.pop();
+        }
+        return w;
+    }
+};
