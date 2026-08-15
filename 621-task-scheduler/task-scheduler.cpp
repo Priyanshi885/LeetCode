1d9437074e1025e4a9a5e25248885a1c689dd3df
Priyanshi Saxena <saxenapriyanshi94@gmail.com>
2026-08-15 14:50:33 +0530

    Time: 43 ms (21.63%) | Memory: 47.8 MB (10.88%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char,int>mp;
+        for(char c:tasks){
+            mp[c]++;
+        }
+        priority_queue<int>pq;
+        for(auto x:mp){
+            pq.push(x.second);
+        }
+        int time=0;
+        while(!pq.empty()){
+            vector<int>temp;
+            for(int i=0; i<=n; i++){
+                if(!pq.empty()){
+                    int freq=pq.top();
+                    pq.pop();
+                    freq--;
+                    if(freq>0){
+                        temp.push_back(freq);
+                    }
+                }
+                time++;
+                if(pq.empty()&&temp.empty()){
+                    break;
+                }
+            }
+            for(int freq:temp){
+                pq.push(freq);
+            }
+        }
+        return time;    
+    }
+};
