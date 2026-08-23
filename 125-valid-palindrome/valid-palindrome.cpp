2bb9732510a5cf33803ea82b87f249351ea94aeb
Priyanshi Saxena <saxenapriyanshi94@gmail.com>
2026-08-23 22:19:19 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (77.52%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+        int n=s.length();
         int left=0;
-        int right = s.size()-1;
-
-        while(left<right){ //keep checking the string until all pairs are compared
-            while(left<right && !isalnum(s[left])){ //keep moving left until you found a valid letter or digit
-                left++;
-            }
-            while(left<right && !isalnum(s[right])){ // keep moving right until you found a valid letter or digit
-                right--;
-            }
-            
-            if(tolower(s[left]) != tolower(s[right])){
-                return false;
-            }
+        int right = n-1;
+        while(left<right){
+        while(left<right && !isalnum(s[left])){
             left++;
+        }
+        while(left<right && !isalnum(s[right])){
             right--;
+        }
+        if(tolower(s[left])!=tolower(s[right]))
+            return false;
+        left++;
+        right--;
         }
         return true;
     }
