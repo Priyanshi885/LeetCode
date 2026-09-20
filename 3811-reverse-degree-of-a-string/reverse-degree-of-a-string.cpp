4d9282a24ddf61af50450084dd3b9d8e5469f061
Priyanshi Saxena <saxenapriyanshi94@gmail.com>
2026-09-20 23:57:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (15.78%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int n=s.size();
+        int answer=0;
+        for(int i=0; i<n; i++){
+            int normal_index=s[i]-'a'+1;
+            int reverse_index=27-normal_index;
+            int string_index=i+1;
+            int product=reverse_index*string_index;
+            answer+=product;
+        }
+        return answer;
+    }
+};
