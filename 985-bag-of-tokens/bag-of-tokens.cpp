edcd0aed35e2d78e5792411bf334aff293b28f77
Priyanshi Saxena <saxenapriyanshi94@gmail.com>
2026-09-23 11:13:01 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (83.56%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.cpp
+++ 985-bag-of-tokens/bag-of-tokens.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
         int n=tokens.size();
-        sort(tokens.begin(),tokens.end());
-
+        
         int i=0;
         int j=n-1;
+
+        sort(tokens.begin(),tokens.end());
 
         int score=0;
         int maxScore=0;
